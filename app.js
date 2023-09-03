90ddda2237dcc53e83de60b0032257a9c667504e
rekha kumari bheel <134380492+Rekhakumari28@users.noreply.github.com>
2023-09-03 17:52:40 +0530

    banana app done

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ function clickHandler() {
        //calling server for processing
 
        fetch(getTranslationURL(inputText))
-              .then(response => response.json())
-              .then(json => {
-                     var translatedText = json.contents.translated;
-                     outputDiv.innerText=translatedText;
-              })
+    .then(response=>response.json())
+    .then(json=> {
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;
+    })
               .catch(errorHandler)
                                    
        };
